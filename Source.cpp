e4e73eab22c31fd55dda805a49b5bf5b2aed644c
Ilya Nyrkov <71572784+IlyaNyrkov@users.noreply.github.com>
2021-03-01 15:00:34 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,8 @@
 #include <Windows.h>
 #include <tchar.h>
 #include <string>
-#define TIMER 1
+#define TIMER1 1 //square
+#define TIMER2 2 //text
 
 #define NORMAL_WINDOW_HEIGHT 720
 #define NORMAL_WINDOW_WIDTH 1280
@@ -15,6 +16,10 @@
 //Второй поток выводит в правую половину окна вводимый текст.
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+DWORD WINAPI FirstThread(LPVOID param);
+DWORD WINAPI SecondThread(LPVOID param);
+HANDLE MutexHwnd = CreateMutex(0, 0, 0);
 
 TCHAR WinName[] = _T("MainFrame");
 
@@ -30,7 +35,6 @@ void PrintText(HDC hdc, const std::wstring text, int x, int y) {
 
 void PrintFigure(int x_pos, int y_pos, int x_size, int y_size,
 	HWND hWnd, Color color, HBRUSH& brush, HDC& hdc) {
-	brush = CreateSolidBrush(RGB(color.r, color.g, color.b));
 	SelectObject(hdc, brush);
 	Rectangle(hdc, x_pos, y_pos, x_size, y_size);
 }
@@ -72,41 +76,43 @@ int WINAPI _tWinMain(HINSTANCE This,		 // Дескриптор текущего 
 		This, 						// Дескриптор приложения 
 		NULL); 					// Дополнительной информации нет 
 
-	ShowWindow(hWnd, mode);
+	ShowWindow(hWnd, mode); 
 
 	// Цикл обработки сообщений 
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		TranslateMessage(&msg); 	
+		DispatchMessage(&msg); 		
 	}
 	return 0;
 }
 
 
-static std::wstring result = L"";
-
+static std::wstring result = L"text";
+static bool is_text_enter = true;
+static bool is_square_moving = true;
+static int y_pos = 0;
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)		 // Обработчик сообщений
 	{
-		HBRUSH hbrush;
-		PAINTSTRUCT ps;
-		HDC hdc;
-		HINSTANCE hInst;
-		static HWND hEdt1;
-		static int y_pos;
-		y_pos = 0;
-		static bool is_working;
-		is_working = false;
+	HINSTANCE hInst;
+	static HWND hEdt1;
+	static wchar_t c[21];
+	static bool is_working;
+	is_working = false;
+
 	case WM_PAINT:
 	{
-		hdc = BeginPaint(hWnd, &ps);
-		Color figure_color = { 0, 255 };
-		PrintFigure(0, y_pos, 100, 100 + y_pos, hWnd, figure_color, hbrush, hdc);
 
-		TextOut(hdc, 200, 200, result.c_str(), result.size());
-		EndPaint(hWnd, &ps);
+		if (is_text_enter) {
+			is_text_enter = false;
+			CreateThread(NULL, 0, SecondThread, hWnd, 0, NULL);
+		}
+		if (is_square_moving) {
+			is_square_moving = false;
+			CreateThread(NULL, 0, FirstThread, hWnd, 0, NULL);
+		}
 		return 0;
 	}
 	case WM_DESTROY:
@@ -114,43 +120,75 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		PostQuitMessage(0);
 		return 0;
 	}
-	case WM_RBUTTONDOWN:
-		if (is_working) {
-			KillTimer(hWnd, TIMER);
-		}
-		else {
-			SetTimer(hWnd, TIMER, 10, NULL);
-		}
-		is_working = !is_working;
 	case WM_TIMER:
 	{
-		if (wParam == TIMER) {
-			if (NORMAL_WINDOW_HEIGHT - 130 == y_pos) {
+		if (LOWORD(wParam) == TIMER1) {
+			if (NORMAL_WINDOW_HEIGHT - 130 <= y_pos) {
 				y_pos = 0;
 			}
 			y_pos++;
-			InvalidateRect(hWnd, NULL, TRUE);
-			SetTimer(hWnd, TIMER, 5, NULL);
-
+			is_square_moving = true;
+			RECT rectangle;
+			rectangle.top = 0;
+			rectangle.right = 100;
+			rectangle.bottom = NORMAL_WINDOW_HEIGHT;
+			rectangle.left = 0;
+			RECT* lpRect = &rectangle;
+			InvalidateRect(hWnd, lpRect, TRUE);
 		}
+		else if (LOWORD(wParam) == TIMER2) {
+			is_text_enter = true;
+			InvalidateRect(hWnd, NULL, 0);
+		}
+		return 0;
 	}
 	case WM_CREATE:
 	{
 		HWND text_enter;
-		HWND text_enter_confirm;
 		text_enter = CreateWindow(L"edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_RIGHT, NORMAL_WINDOW_WIDTH - 300, 200, 200, 20, hWnd, (HMENU)(EDIT_1), NULL, NULL);
+		SetTimer(hWnd, TIMER1, 1, NULL);
+		SetTimer(hWnd, TIMER2, 60, NULL);
 		return 0;
 	}
 	case WM_COMMAND:
 	{
-		wchar_t c[22];
+		
 		GetWindowText((HWND)lParam, c, 21);
 		result = c;
-		InvalidateRect(hWnd, NULL, NULL);
 		return 0;
 	}
-	default:
+	case WM_SIZE:
+	{
+		is_square_moving = true;
+		is_text_enter = true;
+		return 0;
+	}
+	default: 
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
+	return 0;
+}
+
+DWORD __stdcall FirstThread(LPVOID param) {
+	HWND hWnd = static_cast<HWND>(param);
+	PAINTSTRUCT ps;
+	Color figure_color = { 0, 255 };
+	HBRUSH hbrush = CreateSolidBrush(RGB(figure_color.r, figure_color.g, figure_color.b));
+	WaitForSingleObject(MutexHwnd, INFINITY);
+	HDC hdc = BeginPaint(hWnd, &ps);
+	PrintFigure(0, y_pos, 100, 100 + y_pos, hWnd, figure_color, hbrush, hdc);
+	EndPaint(hWnd, &ps);
+	ReleaseMutex(MutexHwnd);
+	return 0;
+}
+
+DWORD __stdcall SecondThread(LPVOID param) {
+	HWND hWnd = static_cast<HWND>(param);
+	PAINTSTRUCT ps;
+	WaitForSingleObject(MutexHwnd, INFINITY);
+	HDC hdc = BeginPaint(hWnd, &ps);
+	TextOut(hdc, 200, 200, result.c_str(),result.size());
+	EndPaint(hWnd, &ps);
+	ReleaseMutex(MutexHwnd);
 	return 0;
 }
