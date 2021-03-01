8314be60c95d1212122b454a1c63f8acdf12b710
Ilya Nyrkov <71572784+IlyaNyrkov@users.noreply.github.com>
2021-03-01 14:58:18 +0300

    Update Program1_source.cpp

--- Program1_source.cpp
+++ Program1_source.cpp
@@ -1,43 +1,21 @@
 #include <Windows.h>
 #include <tchar.h>
-#include <string>
-#define TIMER1 1 //square
-#define TIMER2 2 //text
+#include <Psapi.h>
+#include "Proccess_list.h"
+//лаба 2, задание: 
+//Сформировать два приложения, которые открывают по одному окну.
+//В окне 1 по щелчку левой клавиши мыши: при помощи FindWindow() найти дескриптор окна 2. 
+//Выдать сообщение об этом. Если операция неудачная закрыть приложение 1.
+//При помощи функции SendMessage()  и поля WPARAM  передать свой дескриптор второму окну.
+//Выдать сообщение об этом. В окне 2: при получении сообщения WM_USER+1 (левая клавиша)
+//выдать список запущенных приложений. При нажатии правой клавиши в первом приложении - 
+//сообщение о получении.
 
-#define NORMAL_WINDOW_HEIGHT 720
-#define NORMAL_WINDOW_WIDTH 1280
-#define BUTTN_1 1
-#define EDIT_1 2
-//Первый поток выводит в левую половину окна фигуру
-//плавно перемещающуюся по вертикали от верхнего края 
-//окна до нижнего, скачком возвращается назад и повторяет движение вниз.
-//В каждом шаге y - координату фигуры изменяйте на 1 пиксел.
-//-------------------------------------------
-//Второй поток выводит в правую половину окна вводимый текст.
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-DWORD WINAPI FirstThread(LPVOID param);
-DWORD WINAPI SecondThread(LPVOID param);
-HANDLE MutexHwnd = CreateMutex(0, 0, 0);
+TCHAR WinName[] = _T("Part_1");
 
-TCHAR WinName[] = _T("MainFrame");
-
-struct Color {
-	int r = 0;
-	int g = 0;
-	int b = 0;
-};
-
-void PrintText(HDC hdc, const std::wstring text, int x, int y) {
-	TextOut(hdc, x, y, text.c_str(), text.size());
-}
-
-void PrintFigure(int x_pos, int y_pos, int x_size, int y_size,
-	HWND hWnd, Color color, HBRUSH& brush, HDC& hdc) {
-	SelectObject(hdc, brush);
-	Rectangle(hdc, x_pos, y_pos, x_size, y_size);
-}
 
 int WINAPI _tWinMain(HINSTANCE This,		 // Дескриптор текущего приложения 
 	HINSTANCE Prev, 	// В современных системах всегда 0 
@@ -65,130 +43,68 @@ int WINAPI _tWinMain(HINSTANCE This,		 // Дескриптор текущего 
 
 	// Создание окна 
 	hWnd = CreateWindow(WinName,			// Имя класса окна 
-		_T("Каркас Windows-приложения"), 		// Заголовок окна 
+		_T("Вторая лаба от Бьёрна Страуструпа"), 		// Заголовок окна 
 		WS_OVERLAPPEDWINDOW, 		// Стиль окна 
 		CW_USEDEFAULT,				// x 
 		CW_USEDEFAULT, 				// y	 Размеры окна 
-		NORMAL_WINDOW_WIDTH, 				// width 
-		NORMAL_WINDOW_HEIGHT, 				// Height 
+		CW_USEDEFAULT, 				// width 
+		CW_USEDEFAULT, 				// Height 
 		HWND_DESKTOP, 				// Дескриптор родительского окна 
 		NULL, 						// Нет меню 
 		This, 						// Дескриптор приложения 
 		NULL); 					// Дополнительной информации нет 
 
-	ShowWindow(hWnd, mode); 
+	ShowWindow(hWnd, mode); 				// Показать окно
 
 	// Цикл обработки сообщений 
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
-		TranslateMessage(&msg); 	
-		DispatchMessage(&msg); 		
+		TranslateMessage(&msg); 		// Функция трансляции кодов нажатой клавиши 
+		DispatchMessage(&msg); 		// Посылает сообщение функции WndProc() 
 	}
 	return 0;
 }
 
+// Оконная функция вызывается операционной системой
+// и получает сообщения из очереди для данного приложения
 
-static std::wstring result = L"text";
-static bool is_text_enter = true;
-static bool is_square_moving = true;
-static int y_pos = 0;
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)		 // Обработчик сообщений
 	{
-	HINSTANCE hInst;
-	static HWND hEdt1;
-	static wchar_t c[21];
-	static bool is_working;
-	is_working = false;
-
-	case WM_PAINT:
+	case WM_USER:
 	{
-
-		if (is_text_enter) {
-			is_text_enter = false;
-			CreateThread(NULL, 0, SecondThread, hWnd, 0, NULL);
+		std::wstring proccesses;
+		show_procceses(proccesses);
+		MessageBox(hWnd, proccesses.c_str(), L"Processes_list", NULL);
+		return 0;
+	}
+	case WM_LBUTTONDOWN:
+	{
+		HWND hWnd_2 = FindWindow(L"Part_2", NULL);
+		if (hWnd_2 != NULL) {
+			MessageBox(hWnd, L"Second window handle found, sending the message",
+				L"Handle status", NULL);
+			SendMessage(hWnd_2, WM_USER + 1, (WPARAM)hWnd, NULL);
+			MessageBox(hWnd, L"First window handle sent to second window", L"Handle status", NULL);
 		}
-		if (is_square_moving) {
-			is_square_moving = false;
-			CreateThread(NULL, 0, FirstThread, hWnd, 0, NULL);
+		else {
+			exit(0);
 		}
+		return 0;
+	}
+	case WM_RBUTTONDOWN:
+	{
+		MessageBox(hWnd, L"Processes", L"Running apps list accepted", NULL);
 		return 0;
 	}
 	case WM_DESTROY:
 	{
 		PostQuitMessage(0);
 		return 0;
-	}
-	case WM_TIMER:
-	{
-		if (LOWORD(wParam) == TIMER1) {
-			if (NORMAL_WINDOW_HEIGHT - 130 <= y_pos) {
-				y_pos = 0;
-			}
-			y_pos++;
-			is_square_moving = true;
-			RECT rectangle;
-			rectangle.top = 0;
-			rectangle.right = 100;
-			rectangle.bottom = NORMAL_WINDOW_HEIGHT;
-			rectangle.left = 0;
-			RECT* lpRect = &rectangle;
-			InvalidateRect(hWnd, lpRect, TRUE);
-		}
-		else if (LOWORD(wParam) == TIMER2) {
-			is_text_enter = true;
-			InvalidateRect(hWnd, NULL, 0);
-		}
-		return 0;
-	}
-	case WM_CREATE:
-	{
-		HWND text_enter;
-		text_enter = CreateWindow(L"edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_RIGHT, NORMAL_WINDOW_WIDTH - 300, 200, 200, 20, hWnd, (HMENU)(EDIT_1), NULL, NULL);
-		SetTimer(hWnd, TIMER1, 1, NULL);
-		SetTimer(hWnd, TIMER2, 60, NULL);
-		return 0;
-	}
-	case WM_COMMAND:
-	{
-		
-		GetWindowText((HWND)lParam, c, 21);
-		result = c;
-		return 0;
-	}
-	case WM_SIZE:
-	{
-		is_square_moving = true;
-		is_text_enter = true;
-		return 0;
-	}
-	default: 
+	}// Завершение программы 
+	default: 			// Обработка сообщения по умолчанию 
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
-	return 0;
-}
-
-DWORD __stdcall FirstThread(LPVOID param) {
-	HWND hWnd = static_cast<HWND>(param);
-	PAINTSTRUCT ps;
-	Color figure_color = { 0, 255 };
-	HBRUSH hbrush = CreateSolidBrush(RGB(figure_color.r, figure_color.g, figure_color.b));
-	WaitForSingleObject(MutexHwnd, INFINITY);
-	HDC hdc = BeginPaint(hWnd, &ps);
-	PrintFigure(0, y_pos, 100, 100 + y_pos, hWnd, figure_color, hbrush, hdc);
-	EndPaint(hWnd, &ps);
-	ReleaseMutex(MutexHwnd);
-	return 0;
-}
-
-DWORD __stdcall SecondThread(LPVOID param) {
-	HWND hWnd = static_cast<HWND>(param);
-	PAINTSTRUCT ps;
-	WaitForSingleObject(MutexHwnd, INFINITY);
-	HDC hdc = BeginPaint(hWnd, &ps);
-	TextOut(hdc, 200, 200, result.c_str(),result.size());
-	EndPaint(hWnd, &ps);
-	ReleaseMutex(MutexHwnd);
 	return 0;
 }
